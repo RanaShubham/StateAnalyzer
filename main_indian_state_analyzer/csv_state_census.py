d7c652aee0725840258f9be0c61c98a7ead0fb4f
Shubham Rana <ranashubham7296@gmail.com>
2020-11-22 23:48:59 +0530

    [Shubham Rana]: Added: Ability to write the csv data into json after sorting it by state

--- main_indian_state_analyzer/csv_state_census.py
+++ main_indian_state_analyzer/csv_state_census.py
@@ -1,6 +1,12 @@
+import json
+
 from Indian_state_analyzer.my_csv_analyzer.csv_pojo import PojoCsv
+from Indian_state_analyzer.my_csv_analyzer.csv_analyzer import CsvAnalyzer
 
 class StateCensusCsv(PojoCsv):
+
+    MY_FILE = "../resources/IndiaStateCensusData.csv"
+    SORTED_STATE = "./SortedByState.json"
 
     def __init__(self, ordered_dict):
         self.State = ordered_dict.get("State")
@@ -29,3 +35,41 @@ class StateCensusCsv(PojoCsv):
            :rtype: bool
         '''
         return  self.State and self.Population and self.AreaInSqKm and self.DensityPerSqKm
+
+    @staticmethod
+    def get_object_list():
+        '''
+            To get object data in list form from the csv file using CsvAnalyzer module.
+            :return: List of objects of this type.
+            :rtype: list
+        '''
+        return CsvAnalyzer.read_csv_data(StateCensusCsv.MY_FILE, StateCensusCsv)
+
+    @staticmethod
+    def get_list_sorted_by_state():
+        '''
+            Sorts the list of  objects of this type according to state in ascending order.
+            :return: Sorted object list
+            :rtype: list
+        '''
+        my_list = StateCensusCsv.get_object_list()
+        my_list.sort(key = lambda state : state.State)
+        save_sorted_to_json(my_list)
+
+def save_sorted_to_json(my_list):
+    '''
+        Writes the list of objects of this type in json file.
+        :param my_list: List of objects of this type.
+        :type my_list:  list
+        :return: None
+        :rtype: None
+    '''
+    with open(StateCensusCsv.SORTED_STATE, "w") as file:
+        for each in my_list:
+            file.write(json.dumps(each.__dict__, indent=4))
+
+def driver_function():
+    StateCensusCsv.get_list_sorted_by_state()
+
+if __name__ == "__main__":
+    driver_function()
