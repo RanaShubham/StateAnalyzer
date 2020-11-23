76184106aa35685ba3b2902402bd620f7c4e3eaf
Shubham Rana <ranashubham7296@gmail.com>
2020-11-23 14:18:05 +0530

    [Shubham Rana] Refactored: To improve the test cases.

--- test_indian_state_analyzer/test_us_census_csv.py
+++ test_indian_state_analyzer/test_us_census_csv.py
@@ -1,31 +1,21 @@
 from Indian_state_analyzer.main_indian_state_analyzer.csv_us_census import USCensusCsv
 
 def test_get_list_sorted_by_state_should_return_list_sorted_by_state_name_in_ascending_order():
-    my_list = USCensusCsv.get_list_sorted_by_state()
-    for each in my_list:
-        if (each != my_list[0]):
-            assert each.State > my_list[0].State
+    sorted_object_list = USCensusCsv.get_list_sorted_by_state()
+    assert  sorted_object_list[0].State < sorted_object_list[-1].State
 
-def test_get_list_sorted_by_state_code_should_return_list_sorted_by_state_name_in_ascending_order():
-    my_list = USCensusCsv.get_list_sorted_by_state_code()
-    for each in my_list:
-        if (each != my_list[0]):
-            assert each.State_Id > my_list[0].State_Id
+def test_get_list_sorted_by_state_code_should_return_list_sorted_by_state_Id_in_ascending_order():
+    sorted_object_list = USCensusCsv.get_list_sorted_by_state_code()
+    assert sorted_object_list[0].State_Id < sorted_object_list[-1].State_Id
 
-def test_get_list_sorted_by_state_housing_density_should_return_list_sorted_by_state_name_in_descending_order():
-    my_list = USCensusCsv.get_list_sorted_by_state_housing_density()
-    for each in my_list:
-        if (each != my_list[0]):
-            assert float(each.Housing_Density) < float(my_list[0].Housing_Density)
+def test_get_list_sorted_by_state_housing_density_should_return_list_sorted_by_state_housing_density_in_descending_order():
+    sorted_object_list = USCensusCsv.get_list_sorted_by_state_housing_density()
+    assert float(sorted_object_list[0].Housing_Density) > float(sorted_object_list[-1].Housing_Density)
 
-def test_get_list_sorted_by_state_population_density_should_return_list_sorted_by_state_name_in_descending_order():
-    my_list = USCensusCsv.get_list_sorted_by_state_population_density()
-    for each in my_list:
-        if (each != my_list[0]):
-            assert float(each.Population_Density) < float(my_list[0].Population_Density)
+def test_get_list_sorted_by_state_population_density_should_return_list_sorted_by_population_density_in_descending_order():
+    sorted_object_list = USCensusCsv.get_list_sorted_by_state_population_density()
+    assert float(sorted_object_list[0].Population_Density) > float(sorted_object_list[-1].Population_Density)
 
-def test_get_list_sorted_by_state_size_should_return_list_sorted_by_state_name_in_descending_order():
-    my_list = USCensusCsv.get_list_sorted_by_state_size()
-    for each in my_list:
-        if (each != my_list[0]):
-            assert float(each.Land_area) < float(my_list[0].Land_area)
+def test_get_list_sorted_by_state_size_should_return_list_sorted_by_land_area_in_descending_order():
+    sorted_object_list = USCensusCsv.get_list_sorted_by_state_size()
+    assert float(sorted_object_list[0].Land_area) > float(sorted_object_list[-1].Land_area)
