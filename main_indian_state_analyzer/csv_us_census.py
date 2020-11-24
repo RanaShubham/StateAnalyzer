53863cdcc56e9867a662b5fdb35d85286f5e2c84
Shubham Rana <ranashubham7296@gmail.com>
2020-11-24 18:13:25 +0530

    [Shubham Rana]: Refactored to directly log the sorted list.

--- main_indian_state_analyzer/csv_us_census.py
+++ main_indian_state_analyzer/csv_us_census.py
@@ -4,10 +4,10 @@ import logging
 from Indian_state_analyzer.my_csv_analyzer.csv_analyzer import CsvAnalyzer
 from Indian_state_analyzer.my_csv_analyzer.csv_pojo import CsvHandler
 
-logging.basicConfig(filename='./USCensusTest.log', level=logging.DEBUG, format='%(asctime)s | %(levelname)s | %(message)s')
+logging.basicConfig(filename='./USCensusTest.log', level=logging.DEBUG,
+                    format='%(asctime)s | %(levelname)s | %(message)s')
 
 class USCensusCsvHandler(CsvHandler):
-
     CSV_FILE_UNSORTED = "../resources/USCensusData.csv"
     JSON_FILE_BY_STATE = "../resources/UsCensusData_sortedByState.json"
     JSON_FILE_BY_STATE_CODE = "../resources/UsCensusData_sortedByStateCode.json"
@@ -25,13 +25,26 @@ class USCensusCsvHandler(CsvHandler):
         self.Population_Density = ordered_dict.get("Population Density")
         self.Housing_Density = ordered_dict.get("Housing Density")
 
+    def __repr__(self):
+        return 'State_Id:{}, State:{}, Housing_units:{}, Total_area:{}, Water_area:{},' \
+               ' Land_area:{}, Population_Density:{}, Housing_Density:{}\n'.format(
+            self.State_Id,
+            self.State,
+            self.Housing_units,
+            self.Total_area,
+            self.Water_area,
+            self.Land_area,
+            self.Population_Density,
+            self.Housing_Density)
+
     def display(self):
         '''
             Prints the object data.
             :return: None
             :rtype: None
         '''
-        print('State_Id:{}, State:{}, Housing_units:{}, Total_area:{}, Water_area:{}, Land_area:{}, Population_Density:{}, Housing_Density:{}'.format(
+        print('\nState_Id:{}, State:{}, Housing_units:{}, Total_area:{}, Water_area:{},'
+              ' Land_area:{}, Population_Density:{}, Housing_Density:{}'.format(
             self.State_Id,
             self.State,
             self.Housing_units,
@@ -72,9 +85,9 @@ class USCensusCsvHandler(CsvHandler):
         my_list = USCensusCsvHandler.get_object_list_from_csv()
         my_list.sort(key=lambda state: state.State)
         save_sorted_to_json(my_list, USCensusCsvHandler.JSON_FILE_BY_STATE)
-        for each in my_list:
-            logging.debug(each.State)
-        logging.debug("---------------------------------------------------------------------------------------------------------------------")
+        logging.debug("-"*75+"STATE"+"-"*75)
+        logging.debug(my_list)
+        logging.debug("="*200)
         return my_list
 
     @staticmethod
@@ -87,9 +100,9 @@ class USCensusCsvHandler(CsvHandler):
         my_list = USCensusCsvHandler.get_object_list_from_csv()
         my_list.sort(key=lambda state: state.State_Id)
         save_sorted_to_json(my_list, USCensusCsvHandler.JSON_FILE_BY_STATE_CODE)
-        for each in my_list:
-            logging.debug(each.State_Id)
-        logging.debug("---------------------------------------------------------------------------------------------------------------------")
+        logging.debug("-"*75+"STATE-ID"+"-"*75)
+        logging.debug(my_list)
+        logging.debug("="*200)
         return my_list
 
     @staticmethod
@@ -102,9 +115,9 @@ class USCensusCsvHandler(CsvHandler):
         my_list = USCensusCsvHandler.get_object_list_from_csv()
         my_list.sort(key=lambda state: float(state.Housing_Density), reverse=True)
         save_sorted_to_json(my_list, USCensusCsvHandler.JSON_FILE_BY_MOST_HOUSING_DENSITY)
-        for each in my_list:
-            logging.debug(each.Housing_Density)
-        logging.debug("---------------------------------------------------------------------------------------------------------------------")
+        logging.debug("-"*75+"HOUSING-DENSITY"+"-"*75)
+        logging.debug(my_list)
+        logging.debug("="*200)
         return my_list
 
     @staticmethod
@@ -117,9 +130,9 @@ class USCensusCsvHandler(CsvHandler):
         my_list = USCensusCsvHandler.get_object_list_from_csv()
         my_list.sort(key=lambda state: float(state.Population_Density), reverse=True)
         save_sorted_to_json(my_list, USCensusCsvHandler.JSON_FILE_BY_MOST_POPULATION_DENSITY)
-        for each in my_list:
-            logging.debug(each.Population_Density)
-        logging.debug("----------------------------------------------------------------------------------------------------------------------------")
+        logging.debug("-"*75+"POPULATION-DENSITY"+"-"*75)
+        logging.debug(my_list)
+        logging.debug("="*200)
         return my_list
 
     @staticmethod
@@ -132,17 +145,17 @@ class USCensusCsvHandler(CsvHandler):
         my_list = USCensusCsvHandler.get_object_list_from_csv()
         my_list.sort(key=lambda state: float(state.Land_area), reverse=True)
         save_sorted_to_json(my_list, USCensusCsvHandler.JSON_FILE_BY_STATE_SIZE)
-        for each in my_list:
-            logging.debug(each.Land_area)
-        logging.debug("-------------------------------------------------------------------------------------------------------------")
+        logging.debug("-"*75+"STATE-SIZE"+"-"*75)
+        logging.debug(my_list)
+        logging.debug("="*200)
         return my_list
 
 
 def save_sorted_to_json(sorted_list, sorted_json):
     '''
         Writes the list of objects of this type in json file.
-        :param my_list: List of objects of this type.
-        :type my_list:  list
+        :param sorted_list: List of objects of this type.
+        :type:  list
         :return: None
         :rtype: None
     '''
--- my_csv_analyzer/csv_reader_exception.py
+++ my_csv_analyzer/csv_reader_exception.py
@@ -5,9 +5,18 @@ class ExceptionType(enum.Enum):
     InvalidCsvHeadersException = "File headers unrecognized or pojo class incompatible."
     NoSuchFileException = "File path incorrect."
     InvalidPojoClassException = "Required pojo class not available."
+    UnitMismatchException = "Can't do conversion between unrelated unit type."
+    UnrecognizedUnitException = "Unit type unrecognized"
+    InvalidUnitDataException = "Data type for amount of this unit is invalid."
+    InvalidTemperature = "Data type for temperature is invalid."
     MiscException = "Something went wrong while reading csv."
 
 class CsvReaderException(Exception):
+    def __init__(self, *args):
+        self.type = args[0]
+        self.message = args[1]
+
+class UnitError(Exception):
     def __init__(self, *args):
         self.type = args[0]
         self.message = args[1]
